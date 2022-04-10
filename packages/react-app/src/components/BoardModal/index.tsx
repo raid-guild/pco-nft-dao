ad3cbe0cc1805aecff896f66789593d165c15dbb
Ian-Bright <ianbrighton1@gmail.com>
2022-04-10 07:45:55 -0600

    button component created

---
+++ packages/react-app/src/components/BoardModal/index.tsx
@@ -0,0 +1,78 @@
+import Button from "components/Button";
+import close from "images/close.svg";
+import { capitalize } from "lodash";
+import styled from "styled-components";
+import { BoardSection, SectionStatus } from "views/Game/types";
+
+// type ModalBodyProps = {};
+
+type BoardModalProps = {
+  onClose: () => void;
+  onSectionInteraction: () => void;
+  open: boolean;
+  sectionData: BoardSection;
+};
+
+const ActionContainer = styled.div`
+  display: flex;
+  gap: 81px;
+`;
+
+const Content = styled.div`
+  display: flex;
+`;
+
+const Close = styled.img`
+  cursor: pointer;
+  display: block;
+  height: 24px;
+  margin-left: auto;
+  user-select: none;
+  width: 24px;
+`;
+
+const ModalBody = styled.div`
+  background-color: #c4c4c4;
+  border-radius: 8px;
+  height: 300px;
+  left: 40px;
+  padding: 16px;
+  position: absolute;
+  top: 40px;
+  width: 500px;
+  zindex: 100;
+`;
+
+const Text = styled.div`
+  color: rgba(45, 55, 72, 1);
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 12px;
+  margin-bottom: 8px;
+`;
+
+export default function BoardModal({
+  onClose,
+  onSectionInteraction,
+  open,
+  sectionData,
+}: BoardModalProps): JSX.Element {
+  console.log("SECTION DATA: ", sectionData);
+  if (!open) return <></>;
+  return (
+    <ModalBody>
+      <Close alt="Close" onClick={() => onClose()} src={close} />
+      <Content>
+        <div>
+          <Text>Section ID: {sectionData.id}</Text>
+          <Text>Status: {capitalize(sectionData.status)}</Text>
+        </div>
+      </Content>
+      <ActionContainer>
+        {sectionData.status === SectionStatus.Undiscoverd && (
+          <Button onChange={onSectionInteraction} text="Discover" />
+        )}
+      </ActionContainer>
+    </ModalBody>
+  );
+}
